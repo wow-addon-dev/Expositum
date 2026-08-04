57eed4d00bc676f4dcf05a329db48ae265b56306
Dionys <72759855+diomsg-code@users.noreply.github.com>
2026-08-05 00:05:00 +0200

    v2.19

--- data/CommonData.lua
+++ data/CommonData.lua
@@ -1,7 +1,7 @@
 local addonName, EXT = ...
 
 -- Enable only for releases with new features or important changes.
-EXT.SHOW_UPDATE_NOTICE = true
+EXT.SHOW_UPDATE_NOTICE = false
 
 local expansionBadgePath = "Interface\\AddOns\\" .. addonName .. "\\assets\\expansion-badges\\"
 
